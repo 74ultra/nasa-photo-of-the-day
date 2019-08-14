9e438bdb2607a03943c75b92a8fc178524f41608
74ultra <r.barnhill@protonmail.com>
2019-08-14 14:56:14 -0700

    Initial commit. Image of the day displays with the title above and the description (explanation) below.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+import PhotoCard from './components/PhotoCard.js';
 import "./App.css";
 
 function App() {
+  
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [explanation, setExplanation] = useState("")
+  
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=o7dGJltoP3OqKjtIzhwX7BvD6dVL88BUpFMhUMmp&date=2019-08-10")
+    .then(res => {
+      console.log(res.data);
+      setTitle(res.data.title);
+      setUrl(res.data.url);
+      setExplanation(res.data.explanation);
+    })
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <PhotoCard title={title} 
+                 url={url}
+                 explanation={explanation}/>
     </div>
   );
 }
